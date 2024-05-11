27efb6a49e713a30b7f79c9ea44a80822e907ffd
oscarnavmac <rock.navarrete@hotmail.com>
2024-05-11 17:46:37 -0600

    added: traim.py

---
+++ train.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+def train(model, train_loader, epochs, learning_rate, device):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    model.train()
+
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for inputs, labels in train_loader:
+            # inputs: Una coleccion de imagenes con batch_size tamano de lote
+            # labels: Un vector de dimensionalidad batch_size con enteros denotando la clase para cada imagen
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(inputs)
+
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print(f"Epoch {epoch+1}/{epochs}, Loss: {running_loss / len(train_loader)}")
+
+    return model
+
+        
+def train_KD(teacher, student, train_loader, epochs, learning_rate, T,
+soft_target_loss_weight, ce_loss_weight, device):
+    
+    ce_loss = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(student.parameters(), lr=learning_rate)
+
+    teacher.eval()
+    student.train()
+
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            with torch.no_grad():
+                teacher_logits = teacher(inputs)
+
+            student_logits = student(inputs)
+
+            soft_targets = nn.functional.softmax(teacher_logits / T, dim=-1)
+            soft_prob = nn.functional.log_softmax(student_logits / T, dim=-1)
+
+            soft_target_loss = torch.sum(soft_targets * (soft_targets.log() - soft_prob)) / soft_prob.size()[0] * (T**2)
+
+            label_loss = ce_loss(student_logits, labels)
+
+            loss = soft_target_loss_weight * soft_target_loss + ce_loss_weight * label_loss
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+
+        print(f"{epoch+1}/{epochs}, Loss: {running_loss / len(train_loader)}")
+
+    return student
