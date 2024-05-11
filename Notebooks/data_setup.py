11875850647c796872d142ef379504a0d356044c
oscarnavmac <rock.navarrete@hotmail.com>
2024-05-11 17:32:46 -0600

    added: data_setup.py

---
+++ Notebooks/data_setup.py
@@ -0,0 +1,25 @@
+import torchvision
+import torchvision.transforms as transforms
+
+def create_cifar10_dataloaders(batch_size=4):
+
+    transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                                download=True, transform=transform)
+
+    test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                                download=True, transform=transform)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                            shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
+                                            shuffle=False)
+
+    # Hard-coded classes
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    return train_loader, test_loader, classes
