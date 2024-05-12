ff8555393e3beff99df78e8787dab2b858cb7c14
oscarnavmac <rock.navarrete@hotmail.com>
2024-05-11 19:44:18 -0600

    change data function

--- main.py
+++ main.py
@@ -4,26 +4,11 @@ from student_model import LightNN
 from teacher_model import DeepNN
 from train import train, train_KD
 from eval import test
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Currently running on {device}")
 
-#train_loader, test_loader, classes = create_cifar10_dataloaders()
-
-transforms_cifar = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-
-# Cargando los datos
-train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms_cifar)
-test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms_cifar)
-
-# Dataloaders: usaremos un tamaño de lote de 128
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=2)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=128, shuffle=False, num_workers=2)
+train_loader, test_loader, classes = create_cifar10_dataloaders(128)
 
 torch.manual_seed(42)
 nn_deep = DeepNN(num_classes=10).to(device)
