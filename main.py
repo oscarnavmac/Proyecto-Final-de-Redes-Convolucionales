33818ab1a3245b4b66cc421392b073edc6c146dd
oscarnavmac <rock.navarrete@hotmail.com>
2024-05-11 22:19:12 -0600

    modified main structure

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ from eval import test
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Currently running on {device}")
 
-train_loader, test_loader, classes = create_cifar10_dataloaders(128)
+train_loader, test_loader, classes = create_cifar10_dataloaders(batch_size=128)
+
+teacher = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar10_resnet20", pretrained=True)
+total_params_light = "{:,}".format(sum(p.numel() for p in teacher.parameters()))
+print(f"vgg11 parameters: {total_params_light}")
+test_accuracy_deep = test(teacher, test_loader, device)
 
 torch.manual_seed(42)
 student = LightNN(num_classes=10).to(device)
-train(student, train_loader, epochs=10, learning_rate=0.001, device=device)
+train_KD(teacher=teacher, student=student, train_loader=train_loader, epochs=10, learning_rate=0.001, T=2, soft_target_loss_weight=0.25, ce_loss_weight=0.75, device=device)
 test_accuracy_deep = test(student, test_loader, device)
