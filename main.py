a616d2abc8ce10b6d98322b96d4f4666cbe6c909
oscarnavmac <rock.navarrete@hotmail.com>
2024-05-11 22:13:51 -0600

    modified student

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import torch
 from data_setup import create_cifar10_dataloaders
 from student_model import LightNN
-from teacher_model import DeepNN
 from train import train, train_KD
 from eval import test
 
@@ -11,6 +10,6 @@ print(f"Currently running on {device}")
 train_loader, test_loader, classes = create_cifar10_dataloaders(128)
 
 torch.manual_seed(42)
-nn_deep = DeepNN(num_classes=10).to(device)
-train(nn_deep, train_loader, epochs=10, learning_rate=0.001, device=device)
-test_accuracy_deep = test(nn_deep, test_loader, device)
+student = LightNN(num_classes=10).to(device)
+train(student, train_loader, epochs=10, learning_rate=0.001, device=device)
+test_accuracy_deep = test(student, test_loader, device)
